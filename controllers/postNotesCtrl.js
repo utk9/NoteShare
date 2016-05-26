9c3fca81b05424a7554cde7bab8d76ed3c095d10
utk9 <utkarshlamba.2@gmail.com>
2016-05-26 13:26:43 -0400

    Added file upload validation and added feature to show to user the name of file they have uploaded

--- controllers/postNotesCtrl.js
+++ controllers/postNotesCtrl.js
@@ -2,27 +2,32 @@ angular.module('noteShareApp').controller("postNotesCtrl", ['$scope', 'multipart
 	function($scope, multipartForm) {
 		
 		$scope.newNote = {};
-		$scope.isError = false;
-	
+		$scope.errorMsg = null;
+		$scope.fileNames = [];
+		var numFiles = 0;
+		
 		$('#filesToUpload').on("change", function ()
 		{	
-			$scope.newNote.file = [];
-			$scope.temp = this.files.length;
 			for (var i=0; i<this.files.length; i++){
 				$scope.newNote[i] = this.files[i];
+				$scope.fileNames[i] = this.files[i].name;
 			}
+			numFiles = this.files.length;
+			$scope.$apply();
 		});	
+
 		$scope.uploadData = function(){
 			var uploadURL = '/uploadData';
-			console.log($scope.newNote);
-			if ($scope.newNote.poster!=undefined && $scope.newNote.date!=undefined && 
-				$scope.newNote.date!=undefined && $scope.newNote.course!=undefined){
 
-				multipartForm.post(uploadURL, $scope.newNote);
-		} else {
-			$scope.isError = true;
+			if ($scope.newNote.poster==undefined || $scope.newNote.date==undefined || 
+				$scope.newNote.date==undefined || $scope.newNote.course==undefined){
+				$scope.errorMsg = "Please fill out all the fields";
+		} else if (numFiles < 1){
+			$scope.errorMsg = "Please upload file(s).";
 		}
-
+		else {
+			multipartForm.post(uploadURL, $scope.newNote);
+		}
 	};
 
 
