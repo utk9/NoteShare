526d275a3024030ccedffea9e3eb77f055c8aea7
utk9 <utkarshlamba.2@gmail.com>
2016-05-29 01:01:25 -0400

    Added file viewing support for multiple file types

--- node/tempBackEnd.js
+++ node/tempBackEnd.js
@@ -2,6 +2,8 @@ var express = require('express');
 var mongoose = require('mongoose');
 var fs = require('fs');
 var multer = require('multer');
+var fileType = require('file-type');
+var readChunk = require('read-chunk');
 
 var app = express();
 
@@ -14,28 +16,49 @@ app.use("/views", express.static(__dirname + '/../views'));
 app.use("/css", express.static(__dirname + '/../css'));
 app.use("/images", express.static(__dirname + '/../images'));
 
+app.use("/uploadedFiles", express.static(__dirname + '/..uploadedFiles'));
+app.use("/uploadedImages", express.static(__dirname + '/..uploadedImages'));
+
+
 app.get("/", function(req, res){
 	res.writeHead(200, {'Content-type':'text/html'});
 	fs.createReadStream("../index.html").pipe(res);
 })
+
 app.get(("/uploadedFiles/:name"), function(req, res){
 	
 	var name = req.params.name;
 	fs.readFile("../uploadedFiles/"+name, function (err, content) {
-            if (err) {
-                res.writeHead(400, {'Content-type':'text/html'})
-                console.log(err);
-                res.end("No such image");    
-            } else {
-                res.writeHead(200);
-                res.end(content);
-            }
-        });
+		if (err) {
+			res.writeHead(400, {'Content-type':'text/html'})
+			console.log(err);
+			res.end("No such image");    
+		} else {
+			res.writeHead(200);
+			res.end(content);
+		}
+	});
+	
+});
+
+app.get(("/uploadedImages/:name"), function(req, res){
+	
+	var name = req.params.name;
+	fs.readFile("../uploadedImages/"+name, function (err, content) {
+		if (err) {
+			res.writeHead(400, {'Content-type':'text/html'})
+			console.log(err);
+			res.end("No such image");    
+		} else {
+			res.writeHead(200);
+			res.end(content);
+		}
+	});
 	
 });
 
 
-mongoose.connect("mongodb://localhost/UsersDB", function(err){
+mongoose.connect("mongodb://localhost/NotesDB", function(err){
 	if (err){
 		console.log(err);
 	} else {
@@ -48,20 +71,27 @@ var noteSchema = new mongoose.Schema({
 	date: String,
 	course: String,
 	topic: String,
-	filePaths: [String]
+	filePaths: [String],
+	imagePaths: [String]
 });
 
 var Note = mongoose.model('Note', noteSchema);
 
 var storage = multer.diskStorage({
-  destination: function (req, file, callback) {
-    callback(null, '../uploadedFiles'); 
-  },
-  filename: function (req, file, callback) {
-    var filename = file.fieldname + Date.now();
-    callback(null, filename);
-}
-  });
+	destination: function (req, file, callback) {
+		console.log(file.mimetype);
+		if (file.mimetype == "image/png"){
+			callback(null, '../uploadedImages'); 
+		} else {
+			callback(null, '../uploadedFiles'); 
+		}
+
+	},
+	filename: function (req, file, callback) {
+		var filename = file.fieldname + Date.now();
+		callback(null, filename);
+	}
+});
 var upload = multer({ storage: storage }).any();
 
 //RESTful API:
@@ -89,36 +119,38 @@ app.post('/uploadData', function(req, res){
 		} else {
 			console.log(req.files.length);
 			var mPoster = req.body.poster;
-	var mDate = req.body.date;
-	var mCourse = req.body.course;
-	var mTopic = req.body.topic;
-	var mFilenames= [];
+			var mDate = req.body.date;
+			var mCourse = req.body.course;
+			var mTopic = req.body.topic;
+			var mFilenames= [];
+			var mImagenames = [];
 
-	req.files.forEach(function(file, index){
-		mFilenames.push(file.filename);
-		console.log("added file name: " + file.filename);
-	});
+			req.files.forEach(function(file, index){
+				if (file.mimetype == "image/png"){
+					mImagenames.push(file.filename);
+				} else {
+					mFilenames.push(file.filename);
+				}
+			});
 
-	// mFilenames.push(req.file.filename);
-	// console.log(mFilenames);
-
-	var newNote = new Note({
-			poster: mPoster,
-			date: mDate,
-			course: mCourse,
-			topic: mTopic,
-			filePaths: mFilenames
-		});
+			var newNote = new Note({
+				poster: mPoster,
+				date: mDate,
+				course: mCourse,
+				topic: mTopic,
+				filePaths: mFilenames,
+				imagePaths: mImagenames
+			});
 
 
-	newNote.save(function(err, newNote){
-			if (err){
-  				console.log("Error adding to database");
-  			} else{
-  				console.log("Added to database.");
-  			}
-		});
-	res.end("Uploaded");
+			newNote.save(function(err, newNote){
+				if (err){
+					console.log("Error adding to database");
+				} else{
+					console.log("Added to database.");
+				}
+			});
+			res.end("Uploaded");
 
 		}
 	});
